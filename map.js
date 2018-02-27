12b5896985ff11a369cbb6a11db54e791299ba01
Giada Ferrari <giadaferrari@desktop-vn2dk54.kea.local>
2018-02-27 09:53:47 +0100

    bare json working

--- map.js
+++ map.js
@@ -1,17 +1,71 @@
-let goUsa = $('.usa')
+let goDest = $('.destination')
 
 
-goUsa.click(()=>{
+goDest.click(()=>{
     $('.fay').removeClass('fadeMeIn');
     $('.fay').addClass('fadeMe')
     console.log('working on it')
-    $('.fay').on('animationend', ()=>{
+    $('.fay').on('animationend', ()=> {
 
-fetch('countries.json').then(result => result.json()).then(data => create(data));
+        document.querySelector('.mainMap').style.cssText = "display: none;"
 
-        function create(e){}
+        fetch('countries.json').then(result => result.json()).then(data => create(data));})}
 
 
-})
+        function create(e){
 
-})
+
+                e.forEach((elem)=>{
+            const mainTemplate = document.querySelector('.country').content;
+
+            const postTemplate = document.querySelector('.post').content
+
+            const myClone = mainTemplate.cloneNode(true);
+
+            const postClone = postTemplate.cloneNode(true);
+
+
+        myClone.querySelector('.background').cssText = "background-image: url(" + elem.himage + ")";
+        myClone.querySelector('.name').textContent = elem.country;
+
+
+    console.log(elem.posts)
+
+        elem.posts.forEach((p)=>{
+            p.forEach((post)=>{
+
+                postClone.querySelector('.flag').src = elem.bimage;
+                postClone.querySelector('.number').textContent = p.number;
+                postClone.querySelector('.category').textContent = p.category;
+
+
+                postClone.querySelector('.firstimg').src = post.image
+                postClone.querySelector('.first figcaption').textContent = post.caption;
+                postClone.querySelector('.secondimg').src = post.image
+                postClone.querySelector('.coolSlide').textContent = post.image;
+                postClone.querySelector('.quote').textContent = post.paragraph;
+
+            })
+
+
+        })
+
+        const appendT = document.querySelector('.append');
+        const appendP = myClone.querySelector('.appendPost')
+
+
+        appendP.appendChild(postClone)
+        appendT.appendChild(myClone);
+
+
+
+
+
+})}
+
+
+
+
+
+
+
