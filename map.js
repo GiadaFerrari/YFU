db7b009ee40982a7c257fc9acd8455ef186cab0a
Giada Ferrari <giadaferrari@Giadas-MacBook-Pro.local>
2018-03-06 20:04:38 +0100

    map works

--- map.js
+++ map.js
@@ -1,6 +1,14 @@
+/*map showing*/
 
+$('.usa').hover(()=>{
+   // $('.usaMap').removeClass('hide');
+     $('.usaMap').css('opacity', '1');
+        console.log('I m gonna punch jp');
 
-
+    },()=>{
+  // $('.usaMap').addClass('hide');
+    $('.usaMap').css('opacity', '0');
+    console.log('I love jp');})
 
 
 
@@ -9,88 +17,89 @@
 let goDest = $('.destination')
 
 
-goDest.click((c)=>{
-    $('.fay').removeClass('fadeMeIn');
-    $('.fay').addClass('fadeMe')
-    $('.fay').on('animationend', ()=> {
+goDest.click((c) => {
+        $('.fay').removeClass('fadeMeIn');
+        $('.fay').addClass('fadeMe')
+        $('.fay').on('animationend', () => {
 
-        document.querySelector('.mainMap').style.cssText = "display: none;"
-        let header = document.querySelector('header');
-        header.nextElementSibling.classList.add('whiteMe')
-        /*opacity on scroll*/
-let heroImg = $('.whiteMe');
-let range = heroImg.outerHeight;
+            document.querySelector('.mainMap').style.cssText = "display: none;"
+            let header = document.querySelector('header');
+            header.nextElementSibling.classList.add('whiteMe')
+            /*opacity on scroll*/
+            let heroImg = $('.whiteMe');
+            let range = heroImg.outerHeight;
 
-$(window).scroll(function() {
-  var scrollTop = $(this).scrollTop();
+            $(window).scroll(function () {
+                var scrollTop = $(this).scrollTop();
 
-  $('.whiteMe').css({
-    opacity: function() {
-      var elementHeight = $(this).height();
-      return 1 - (elementHeight - scrollTop) / elementHeight;
-    }
-  });
-});
+                $('.whiteMe').css({
+                    opacity: function () {
+                        var elementHeight = $(this).height();
+                        return 1 - (elementHeight - scrollTop) / elementHeight;
+                    }
+
+                });
+            });
 
 
-        fetch('countries.json').then(result => result.json()).then(data => create(data));})
+            fetch('countries.json').then(result => result.json()).then(data => create(data));
+        })
         let clicked = c.target
 
 
-    console.log(clicked)
-     function create(elem){
-           if($(clicked).hasClass('usa')){
+        function create(elem) {
+            if ($(clicked).hasClass('usa')) {
 
 
-            const mainTemplate = document.querySelector('.country').content;
+                const mainTemplate = document.querySelector('.country').content;
 
-            const postTemplate = document.querySelector('.post').content
+                const postTemplate = document.querySelector('.post').content
 
-            const myClone = mainTemplate.cloneNode(true);
-
-
-
-
-        myClone.querySelector('.background').style.cssText = "background-image: url(" + elem[0].himage + ")";
-        myClone.querySelector('.name').textContent = elem[0].country;
+                const myClone = mainTemplate.cloneNode(true);
 
 
 
-        elem[0].posts.forEach((p)=>{
 
-                  let postClone = postTemplate.cloneNode(true);
-
-            p.forEach((post)=>{
-                postClone.querySelector('.flag').style.cssText = "background-image: url(" + elem[0].bimage + ")";
-                postClone.querySelector('.number').textContent = post.number;
-                postClone.querySelector('.category').textContent = post.category;
-                console.log(p[0])
-                postClone.querySelector('.firstimg').src = p[0].image
-                postClone.querySelector('.first figcaption').textContent = p[0].caption;
-                postClone.querySelector('.secondimg').src = p[1].image
-                postClone.querySelector('.coolSlide').textContent = p[1].caption;
-                postClone.querySelector('.quote').textContent = p[2].paragraph;
+                myClone.querySelector('.background').style.cssText = "background-image: url(" + elem[0].himage + ")";
+                myClone.querySelector('.name').textContent = elem[0].country;
 
 
-            })
+                    let flagN = 0
 
-            let appendP = myClone.querySelector('.appendPost');
+                elem[0].posts.forEach((p) => {
 
-            appendP.appendChild(postClone)
+                    let postClone = postTemplate.cloneNode(true);
+                    flagN++
+                    console.log(flagN)
+                    postClone.querySelector('.flag').classList.add('flag'+flagN)
+                    p.forEach((post) => {
 
-        })
-            const appendT = document.querySelector('.append');
 
-        appendT.appendChild(myClone);
-}}}
+                        postClone.querySelector('.flag').style.cssText = "background-image: url(" + elem[0].bimage + ")";
+                        postClone.querySelector('.number').textContent = post.number;
+                        postClone.querySelector('.category').textContent = post.category;
+                        console.log(p[0])
+                        postClone.querySelector('.firstimg').src = p[0].image
+                        postClone.querySelector('.first figcaption').textContent = p[0].caption;
+                        postClone.querySelector('.secondimg').src = p[1].image
+                        postClone.querySelector('.coolSlide').textContent = p[1].caption;
+                        postClone.querySelector('.quote').textContent = p[2].paragraph;
+
+
+                    })
+
+                    let appendP = myClone.querySelector('.appendPost');
+
+                    appendP.appendChild(postClone)
+
+                })
+                const appendT = document.querySelector('.append');
+
+                appendT.appendChild(myClone);
+            }
+        }
+    }
 
 
 
 )
-
-
-
-
-
-
-
